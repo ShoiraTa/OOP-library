5cfef0bdc64075856e946850d6dd813d9c334ad3
Timo <timowester@gmail.com>
2022-02-09 13:44:42 +0100

    Add teacher test

---
+++ spec/teacher_class_spec.rb
@@ -0,0 +1,22 @@
+require './teacher_class'
+
+require 'yaml'
+
+describe Teacher do
+
+  before :each do
+    @Teacher = Teacher.new specialization:'Math', age:44, name:'herbert'
+  end
+
+  it 'mike is an instance of student' do
+    expect(@Teacher.name).to eq 'herbert'
+  end
+
+  it 'if age is correct' do
+    expect(@Teacher.age).to eq 44
+  end
+
+  it 'if specialization is correct' do
+    expect(@Teacher.specialization).to eq 'Math'
+  end
+end
